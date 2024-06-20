a8109136156a1223a621a8715c5979d8554fde0e
NightmareX136 <148441445+NightmareX136@users.noreply.github.com>
2024-06-20 22:28:46 +0100

    opção sencibilidade

--- MonstersBattleground/Assets/scripts/Server/NetworkManagerUI.cs
+++ MonstersBattleground/Assets/scripts/Server/NetworkManagerUI.cs
@@ -5,28 +5,16 @@ public class NetworkManagerUI : MonoBehaviour
 {
     public void StartServer()
     {
-<<<<<<< Updated upstream
         NetworkManager.Singleton.StartServer();//inicia o servidor
-=======
-        NetworkManager.Singleton.StartServer();
->>>>>>> Stashed changes
     }
 
     public void StartHost()
     {
-<<<<<<< Updated upstream
         NetworkManager.Singleton.StartHost(); //inicia o host
-=======
-        NetworkManager.Singleton.StartHost();
->>>>>>> Stashed changes
     }
 
     public void StartClient()
     {
-<<<<<<< Updated upstream
         NetworkManager.Singleton.StartClient(); //inicia o cliente
-=======
-        NetworkManager.Singleton.StartClient();
->>>>>>> Stashed changes
     }
 }
