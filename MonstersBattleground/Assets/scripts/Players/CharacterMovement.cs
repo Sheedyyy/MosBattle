ebf2bb59ab7ab17d00693582d5da14b7a4f1a0f3
Sheedyyy <202300672@estudantes.ips.pt>
2024-06-27 11:48:24 +0100

    server a melhorar

---
+++ MonstersBattleground/Assets/scripts/Players/CharacterMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class CharacterMovement : NetworkBehaviour
+{
+    #region Variables
+
+    [Header("Chamada do playerSatus")]
+    [SerializeField] private PlayerStatus _playerStatus;
+
+    [Header("Animacoes")]
+    private bool isWalking;
+
+    #endregion
+
+    #region Movimento
+
+    private void Update()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        
+    }
+
+    #endregion
+}
