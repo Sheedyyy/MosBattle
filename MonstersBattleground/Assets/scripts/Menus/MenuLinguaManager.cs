d6cb538c71320d3a3cdf08ce5d0547a615e8b319
Heptoguy <149150463+Heptoguy@users.noreply.github.com>
2024-07-22 21:28:34 +0100

    Lingua continuacao
    ta quase :)

--- MonstersBattleground/Assets/scripts/Menus/MenuLinguaManager.cs
+++ MonstersBattleground/Assets/scripts/Menus/MenuLinguaManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Localization.Settings;
 public class MenuLinguaManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        //salvar a lingua escolhida
+        int ID = PlayerPrefs.GetInt("LocateKey", 0);
+        buttonStuff(ID);
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    //
+    private bool active = false;
+
+    //
+    public void buttonStuff(int LocaleID)
     {
-        
+        // se o active for verdade retorna
+        if (active == true)
+            return;
+        StartCoroutine(setLocale(LocaleID));
     }
+
+
+    //
+    IEnumerator setLocale(int _localID)
+    {
+        //
+        active = true;
+
+        //
+        yield return LocalizationSettings.InitializationOperation;
+
+        //
+        LocalizationSettings.SelectedLocale=LocalizationSettings.AvailableLocales.Locales[ _localID];
+
+        //
+        PlayerPrefs.SetInt("LocaleKey", _localID);
+
+        //
+        active = false;
+    }
+
 }
